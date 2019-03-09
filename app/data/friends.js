407191435cebb1311fcb1d24634c433de2c029de
corbin.felsheim@gmail.com <corbinfelsheim@Corbins-MacBook-Pro.local>
2019-03-09 08:23:20 -0800

    with sample data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,18 @@
-var friends = [];
+var friends = [{
+    name: "Corbin",
+    photo: "https://img-aws.ehowcdn.com/340x221p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/e717ff14cdba4c308e8f1019273abe5c.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+}];
 
 module.exports = {friends: friends}
